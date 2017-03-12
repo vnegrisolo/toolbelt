50ef688367de9c6769619d79479736743bc8feb0
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2017-03-12 20:57:16 +0000

    Configure coverall for umbrella project

--- mix.exs
+++ mix.exs
@@ -7,12 +7,14 @@ defmodule Toolbelt.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
+      test_coverage: [tool: ExCoveralls],
    ]
   end
 
   defp deps do
     [
       {:credo, "~> 0.5", only: [:dev, :test]},
+      {:excoveralls, "~> 0.6", only: :test},
     ]
   end
 end
