2e7493cd86704c76047d7fb34c9f8e99eadc6605
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2017-03-12 21:01:30 +0000

    Configure ex_doc for umbrella project

--- mix.exs
+++ mix.exs
@@ -13,8 +13,10 @@ defmodule Toolbelt.Mixfile do
 
   defp deps do
     [
-      {:credo, "~> 0.5", only: [:dev, :test]},
-      {:excoveralls, "~> 0.6", only: :test},
+      {:credo,       "~> 0.5",   only: [:dev, :test]},
+      {:earmark,     "~> 1.0.0", only: :dev},
+      {:ex_doc,      "~> 0.14",  only: :dev},
+      {:excoveralls, "~> 0.6",   only: :test},
     ]
   end
 end
